07704231bca1ea7a2041318ec0dd62b4097a35eb
rashmiraj513 <65858566+rashmiraj513@users.noreply.github.com>
2020-06-28 17:56:35 +0530

    Update plotData.m

--- plotData.m
+++ plotData.m
@@ -3,10 +3,8 @@ function A = plotData(X, y)
   % plotData(x,y) plots the data points and gives the figure axes labels of population and profit.
   
   % =================YOUR CODE HERE===================
-  % Instructions: Plot the training data into a figure using the "figure" and 
-  % "plot" commands. Set the axes labels using the "xlabel" and "ylabel" commands.
-  % Assume the population and revenue data have been passed in as the x and y 
-  % arguments of this function.
+  % Instructions: Plot the training data into a figure using the "figure" and "plot" commands. Set the axes labels using the "xlabel" and "ylabel" commands.
+  % Assume the population and revenue data have been passed in as the x and y arguments of this function.
   
   figure(1) = plo(X,y,'rx','MarkerSize',10);    % open a new figure window.
   xlabel('Population of City in 10,000s');
@@ -14,9 +12,7 @@ function A = plotData(X, y)
   axis([4 24 -5 25]);
   legend('Training Data');
 
-  % Hint: You can use the 'rx' option with plot to have the markers
-  %       appear as red crosses. Furthermore, you can make the
-  %       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
+  % Hint: You can use the 'rx' option with plot to have the markers appear as red crosses. Furthermore, you can make the markers larger by using plot(..., 'rx', 'MarkerSize', 10);
 
   % ============================================================
 
