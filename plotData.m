b0230fb1b33855a6e653aa7f55c1349dac805363
Rashmi Raj <65858566+rashmiraj513@users.noreply.github.com>
2020-07-01 12:00:14 +0530

    Update plotData.m

--- plotData.m
+++ plotData.m
@@ -1,19 +1,42 @@
 function A = plotData(X, y)
-  % plotData plots the data points x and y into a new figure.
-  % plotData(x,y) plots the data points and gives the figure axes labels of population and profit.
-  
-  % =================YOUR CODE HERE===================
-  % Instructions: Plot the training data into a figure using the "figure" and "plot" commands. Set the axes labels using the "xlabel" and "ylabel" commands.
-  % Assume the population and revenue data have been passed in as the x and y arguments of this function.
-  
-  figure(1) = plo(X,y,'rx','MarkerSize',10);    % open a new figure window.
-  xlabel('Population of City in 10,000s');
-  ylabel('Profit in $10,000s');
-  axis([4 24 -5 25]);
-  legend('Training Data');
+%PLOTDATA Plots the data points x and y into a new figure 
+%   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
+%   population and profit.
+data = load('ex1data1.txt');
+% storing the values of first column in x.
+x = data(:,1);
+% storing the values of second column in y.
+y = data(:,2);
+% calculating the length of y and storing in m.
+m = length(y);
+% creating a matrix whose first column contains only 1 and second column is x.
+X = [ones(m, 1) x];
+theta = zeros(2,1);
+alpha = 0.01;
+iterations = 1500;
+theta = gradientDescent(X,y,theta,alpha,iterations);
 
-  % Hint: You can use the 'rx' option with plot to have the markers appear as red crosses. Furthermore, you can make the markers larger by using plot(..., 'rx', 'MarkerSize', 10);
+figure;       % open a new figure window.
+plot(x,y,'rx','MarkerSize',10); 
+hold on;
+plot(x,X * theta,'b');
+legend('Training Data','Linear Regression');
+axis([4 25 -5 25]);
+title('Population vs Profit');
+xlabel('Population of City in 10,000s');
+ylabel('Profit in $10,000s');
 
-  % ============================================================
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the training data into a figure using the 
+%               "figure" and "plot" commands. Set the axes labels using
+%               the "xlabel" and "ylabel" commands. Assume the 
+%               population and revenue data have been passed in
+%               as the x and y arguments of this function.
+%
+% Hint: You can use the 'rx' option with plot to have the markers
+%       appear as red crosses. Furthermore, you can make the
+%       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
+
+% ============================================================
 
 end;
